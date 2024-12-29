9c00fa2b15620eaad9c2e673cee955151b171fa2
jy95 <jacques.yakoub@gmail.com>
2024-12-29 01:14:58 +0100

    feat: add Configuration (WIP)

--- src/main/java/jy95/fhir/r4/dosage/utils/classes/AbstractTranslator.java
+++ src/main/java/jy95/fhir/r4/dosage/utils/classes/AbstractTranslator.java
@@ -4,7 +4,13 @@ import org.hl7.fhir.r4.model.Dosage;
 import jy95.fhir.r4.dosage.utils.types.DisplayOrder;
 
 public abstract class AbstractTranslator {
+
+    // To turn a dosage field into a human-representation
     public abstract String convert(Dosage dosage);
+
+    // To see if a dosage field is present and thus being turned to String
     public abstract boolean isPresent(Dosage dosage);
+
+    // To debug with field this translator covers
     public abstract DisplayOrder getField();
 }
---
+++ src/main/java/jy95/fhir/r4/dosage/utils/config/FDUConfig.java
@@ -0,0 +1,50 @@
+package jy95.fhir.r4.dosage.utils.config;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Locale;
+import java.util.List;
+import jy95.fhir.r4.dosage.utils.types.DisplayOrder;
+
+// To provide a configuration with the
+@Getter
+@Builder(toBuilder = true)
+public class FDUConfig {
+    /*
+    * Locale to return the humanized dosage string
+    * Default : English
+    * */
+    @Builder.Default private Locale locale = Locale.getDefault();
+    /**
+     * Control the display order used by the algorithm
+     * Useful when you want to turn on / off some specific rules for some reason
+     */
+    @Builder.Default private List<DisplayOrder> displayOrder = List.of(
+            DisplayOrder.METHOD,
+            DisplayOrder.DOSE_QUANTITY,
+            DisplayOrder.DOSE_RANGE,
+            DisplayOrder.RATE_RATIO,
+            DisplayOrder.RATE_QUANTITY,
+            DisplayOrder.RATE_RANGE,
+            DisplayOrder.DURATION_DURATION_MAX,
+            DisplayOrder.FREQUENCY_FREQUENCY_MAX_PERIOD_PERIOD_MAX,
+            DisplayOrder.OFFSET_WHEN,
+            DisplayOrder.DAY_OF_WEEK,
+            DisplayOrder.TIME_OF_DAY,
+            DisplayOrder.ROUTE,
+            DisplayOrder.SITE,
+            DisplayOrder.AS_NEEDED,
+            DisplayOrder.BOUNDS_DURATION,
+            DisplayOrder.BOUNDS_PERIOD,
+            DisplayOrder.BOUNDS_RANGE,
+            DisplayOrder.COUNT_COUNT_MAX,
+            DisplayOrder.EVENT,
+            DisplayOrder.CODE,
+            DisplayOrder.MAX_DOSE_PER_PERIOD,
+            DisplayOrder.MAX_DOSE_PER_ADMINISTRATION,
+            DisplayOrder.MAX_DOSE_PER_LIFETIME,
+            DisplayOrder.ADDITIONAL_INSTRUCTION,
+            DisplayOrder.PATIENT_INSTRUCTION
+    );
+}
