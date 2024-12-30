3356924e9827f371e9245f7570b59baff9064f14
jy95 <jacques.yakoub@gmail.com>
2024-12-30 23:28:50 +0100

    feat: As needed

--- src/main/java/jy95/fhir/r4/dosage/utils/miscellaneous/Translators.java
+++ src/main/java/jy95/fhir/r4/dosage/utils/miscellaneous/Translators.java
@@ -32,7 +32,8 @@ public class Translators {
                         Map.entry(DisplayOrder.TIMING_REPEAT_EXTENSION, new TimingRepeatExtension(config)),
                         Map.entry(DisplayOrder.MODIFIER_EXTENSION,new ModifierExtension(config)),
                         Map.entry(DisplayOrder.TIMING_MODIFIER_EXTENSION, new TimingModifierExtension(config)),
-                        Map.entry(DisplayOrder.ADDITIONAL_INSTRUCTION, new AdditionalInstruction(config))
+                        Map.entry(DisplayOrder.ADDITIONAL_INSTRUCTION, new AdditionalInstruction(config)),
+                        Map.entry(DisplayOrder.AS_NEEDED, new AsNeeded(config))
                 )
         );
         this.bundleControl = new MultiResourceBundleControl(
---
+++ src/main/java/jy95/fhir/r4/dosage/utils/translators/AsNeeded.java
@@ -0,0 +1,51 @@
+package jy95.fhir.r4.dosage.utils.translators;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import jy95.fhir.r4.dosage.utils.functions.ListToString;
+import jy95.fhir.r4.dosage.utils.classes.AbstractTranslator;
+import org.hl7.fhir.r4.model.Dosage;
+import jy95.fhir.r4.dosage.utils.config.FDUConfig;
+
+public class AsNeeded extends AbstractTranslator {
+
+    public AsNeeded(FDUConfig config) {
+        super(config);
+    }
+
+    @Override
+    public CompletableFuture<String> convert(Dosage dosage) {
+        var bundle = getResources();
+
+        // Simple case - only "as-needed"
+        if (dosage.hasAsNeededBooleanType()) {
+            return CompletableFuture.supplyAsync(() -> bundle.getString("fields.asNeeded"));
+        }
+
+        // Complex case - "as-need" for ...
+        var code = dosage.getAsNeededCodeableConcept();
+        var msg = bundle.getString("fields.asNeededFor");
+        var codeAsText = this
+                .getConfig()
+                .getFromCodeableConceptToString()
+                .apply(code);
+
+        return codeAsText
+                .thenApplyAsync(v -> ListToString.convert(bundle, List.of(v)))
+                .thenApplyAsync(v -> MessageFormat.format(msg, v, 1));
+    }
+
+    @Override
+    public boolean isPresent(Dosage dosage) {
+        // No as-needed field(s) ? Not as-needed
+        if (!dosage.hasAsNeeded()) {
+            return false;
+        }
+
+        // Explicitly false ? Not as-needed
+        // AsNeededCodeableConcept present ? As-needed
+        return !dosage.hasAsNeededBooleanType() || dosage.getAsNeededBooleanType().booleanValue();
+    }
+}
