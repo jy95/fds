43af5da508327cbf3e4cbeab70dcd85416b0bc29
jy95 <jacques.yakoub@gmail.com>
2025-01-04 16:01:56 +0100

    feat: RateRange

--- src/main/java/jy95/fhir/r4/dosage/utils/miscellaneous/Translators.java
+++ src/main/java/jy95/fhir/r4/dosage/utils/miscellaneous/Translators.java
@@ -47,7 +47,8 @@ public class Translators {
                         Map.entry(DisplayOrder.COUNT_COUNT_MAX, new CountCountMax(config)),
                         Map.entry(DisplayOrder.DOSE_QUANTITY, new DoseQuantity(config)),
                         Map.entry(DisplayOrder.DOSE_RANGE, new DoseRange(config)),
-                        Map.entry(DisplayOrder.RATE_QUANTITY, new RateQuantity(config))
+                        Map.entry(DisplayOrder.RATE_QUANTITY, new RateQuantity(config)),
+                        Map.entry(DisplayOrder.RATE_RANGE, new RateRange(config))
                 )
         );
         this.bundleControl = new MultiResourceBundleControl(
