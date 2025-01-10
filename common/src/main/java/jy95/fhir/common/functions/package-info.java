2fefe758962cdf83a5b19c543da53545fcfe70d4
jy95 <jacques.yakoub@gmail.com>
2025-01-10 19:50:42 +0100

    docs: Javadoc common module

---
+++ common/src/main/java/jy95/fhir/common/functions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Functional utilities and helpers for processing FHIR data.
+ * Contains reusable methods and functional interfaces for
+ * transforming and querying FHIR resources.
+ */
+package jy95.fhir.common.functions;
---
+++ common/src/main/java/jy95/fhir/common/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Core functionality and shared utilities for the FHIR-related library.
+ * This package contains foundational classes and interfaces
+ * that are used across other packages.
+ */
+package jy95.fhir.common;
