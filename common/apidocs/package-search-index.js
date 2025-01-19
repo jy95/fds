9504fff35f7a4d6f1ea65b5034379943fbd25359
jy95 <jy95@users.noreply.github.com>
2025-01-19 19:41:03 +0000

    deploy: c0f0f1e45b29efdbf40b27536e0a65709dd2b9ff

---
+++ common/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.jy95.fds.common"},{"l":"io.github.jy95.fds.common.bundle"},{"l":"io.github.jy95.fds.common.config"},{"l":"io.github.jy95.fds.common.functions"},{"l":"io.github.jy95.fds.common.translators"},{"l":"io.github.jy95.fds.common.types"}];updateSearchResults();
---
+++ r4/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.jy95.fds.r4"},{"l":"io.github.jy95.fds.r4.config"},{"l":"io.github.jy95.fds.r4.functions"},{"l":"io.github.jy95.fds.r4.translators"},{"l":"io.github.jy95.fds.r4.utils"}];updateSearchResults();
