98be4c199fd032fdd6d83799f0650f7c9fadb3fb
jy95 <jacques.yakoub@gmail.com>
2025-02-09 14:23:44 +0100

    perf: Units of Time formatter, instead of managing properties

---
+++ common/src/main/java/io/github/jy95/fds/common/functions/UnitsOfTimeFormatter.java
@@ -0,0 +1,58 @@
+package io.github.jy95.fds.common.functions;
+
+import com.ibm.icu.number.NumberFormatter;
+import com.ibm.icu.util.MeasureUnit;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Class to format units of time
+ */
+public final class UnitsOfTimeFormatter {
+
+    /**
+     * Map each code of <a href="https://build.fhir.org/valueset-units-of-time.html">Units of Time</a> to their ICU4J units
+     */
+    private static final Map<String, MeasureUnit> UNIT_MAPPING = Map.ofEntries(
+            Map.entry("ms", MeasureUnit.MILLISECOND),
+            Map.entry("s", MeasureUnit.SECOND),
+            Map.entry("min", MeasureUnit.MINUTE),
+            Map.entry("h", MeasureUnit.HOUR),
+            Map.entry("d", MeasureUnit.DAY),
+            Map.entry("wk", MeasureUnit.WEEK),
+            Map.entry("mo", MeasureUnit.MONTH),
+            Map.entry("a", MeasureUnit.YEAR)
+    );
+
+    /**
+     * Formats a time unit with a count (e.g., "3 heures", "1 minute").
+     * @param locale The locale expected for the resulting text
+     * @param unit The <a href="https://build.fhir.org/valueset-units-of-time.html">unit code</a>
+     * @param count The quantity
+     * @return A formatted time-unit string with a count
+     */
+    public static String formatWithCount(Locale locale, String unit, Number count) {
+        return NumberFormatter
+                .withLocale(locale)
+                .unit(UNIT_MAPPING.get(unit))
+                .format(count)
+                .toString();
+    }
+
+    /**
+     * Formats a time unit without a count (e.g., "heures", "minute").
+     * @param locale The locale expected for the resulting text
+     * @param unit The <a href="https://build.fhir.org/valueset-units-of-time.html">unit code</a>
+     * @param count The quantity
+     * @return A formatted time-unit string without a count
+     */
+    public static String formatWithoutCount(Locale locale, String unit, Number count) {
+        String formattedWithCount = formatWithCount(locale, unit, count);
+        // Remove the numeric portion and return only the unit text
+        // ICU4J allows us to format with unit, but we use only the textual part in the final output.
+        int indexOfSpace = formattedWithCount.indexOf(" ");
+
+        return formattedWithCount.substring(indexOfSpace + 1);
+    }
+}
