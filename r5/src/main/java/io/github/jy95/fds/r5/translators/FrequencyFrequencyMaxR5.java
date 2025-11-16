a80a3fe2d65f013038c32cc768101a3f0cf4d4d5
jy95 <jacques.yakoub@gmail.com>
2025-11-16 23:22:46 +0100

    refactor: FrequencyFrequencyMax

--- r5/src/main/java/io/github/jy95/fds/r5/translators/FrequencyFrequencyMaxR5.java
+++ r5/src/main/java/io/github/jy95/fds/r5/translators/FrequencyFrequencyMaxR5.java
@@ -3,11 +3,10 @@ package io.github.jy95.fds.r5.translators;
 import io.github.jy95.fds.common.functions.TranslationService;
 import io.github.jy95.fds.common.translators.FrequencyFrequencyMax;
 import io.github.jy95.fds.r5.config.FDSConfigR5;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import org.hl7.fhir.r5.model.Dosage;
-
-import java.util.Map;
 
 /**
  * R5 class for translating "timing.repeat.frequency" / "timing.repeat.frequencyMax"
@@ -15,40 +14,16 @@ import java.util.Map;
  * @author jy95
  */
 @RequiredArgsConstructor
-public class FrequencyFrequencyMaxR5 implements FrequencyFrequencyMax<Dosage> {
+public class FrequencyFrequencyMaxR5 implements FrequencyFrequencyMax<Dosage, FDSConfigR5> {
 
     /** Translation service */
+    @Getter
     private final TranslationService<FDSConfigR5> translationService;
 
     /** {@inheritDoc} */
     @Override
     public boolean hasTiming(Dosage dosage) {
         return dosage.hasTiming();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public String formatFrequencyAndFrequencyMaxText(int frequencyMin, int frequencyMax) {
-        Map<String, Object> arguments = Map.of(
-                "frequency", frequencyMin,
-                "maxFrequency", frequencyMax
-        );
-        var frequencyAndFrequencyMaxMsg = translationService.getMessage(KEY_FREQUENCY_AND_FREQUENCY_MAX);
-        return frequencyAndFrequencyMaxMsg.format(arguments);
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public String formatFrequencyMaxText(int frequencyMax) {
-        var frequencyMaxMsg = translationService.getMessage(KEY_FREQUENCY_MAX);
-        return frequencyMaxMsg.format(new Object[]{frequencyMax});
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public String formatFrequencyText(int frequency) {
-        var frequencyMsg = translationService.getMessage(KEY_FREQUENCY);
-        return frequencyMsg.format(new Object[]{frequency});
     }
 
     /** {@inheritDoc} */
