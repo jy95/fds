2d7347b404fefcab97c01a05431a8d662cebb79f
jy95 <jacques.yakoub@gmail.com>
2026-01-01 23:06:10 +0100

    feat: translate l10n bundles to es

--- common/src/test/java/io/github/jy95/fds/translators/AbstractBoundsPeriodTest.java
+++ common/src/test/java/io/github/jy95/fds/translators/AbstractBoundsPeriodTest.java
@@ -85,6 +85,7 @@ public abstract class AbstractBoundsPeriodTest<C extends FDSConfig, D> extends A
         } else if (locale.equals(Locale.GERMAN)) {
             assertEquals("bis 07.02.2015, 13:28:17", actual);
         } else if (locale.equals(Locale.forLanguageTag("es"))) {
+            System.out.println(actual);
             assertTrue(actual.startsWith("hasta 7 feb 2015"));
             assertTrue(actual.contains("13:28:17"));
         } else {
